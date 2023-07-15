8d59905397b80b342655876abe341513be3e00c6
lobo <daniel@dlobo.org>
2023-07-14 22:28:29 -0300

    styles but no API yet

--- pages/index.js
+++ pages/index.js
@@ -5,68 +5,35 @@ export default function Home({ isConnected }) {
   return (
     <div className="container">
       <Head>
-        <title>Gasparin</title>
+        <title>Wimpy Kid</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
+          {/* Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a> */}
+          Wimpy Kid
         </h1>
 
         {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
+          <h2 className="subtitle">Connected! ✅</h2>
         ) : (
           <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
+            {/* You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions. */}
+            No connection ❌
           </h2>
         )}
 
-        <p className="description">
+        {/* <p className="description">
           Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        </p> */}
+      <textarea className='textinput' placeholder='Enter message...'></textarea>
+      <button style={{ width: '100%', display: 'block' }} type="submit">Send! 🚀</button>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        "I solemnly swear that I am up to no good"
       </footer>
 
       <style jsx>{`
@@ -202,6 +169,15 @@ export default function Home({ isConnected }) {
             flex-direction: column;
           }
         }
+
+        .textinput {
+          float: left;
+          width: 100%;
+          min-height: 75px;
+          outline: none;
+          resize: none;
+          border: 1px solid grey;
+      }
       `}</style>
 
       <style jsx global>{`
